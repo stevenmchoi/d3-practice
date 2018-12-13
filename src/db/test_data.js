5a427753a1d2ca66cb97a7526f777d14b9bfb052
Steven Choi <steven.m.choi@gmail.com>
2018-12-13 09:51:08 -0800

    Use d3-timelines instead of event-drops

---
+++ src/db/test_data.js
@@ -0,0 +1,17 @@
+export default [
+	{
+		times: [
+			{ starting_time: 1355752800000, ending_time: 1355752800000 },
+			{ starting_time: 1355767900000, ending_time: 1355767900000 },
+		],
+	},
+	{
+		times: [{ starting_time: 1355759910000, ending_time: 1355759910000 }],
+	},
+	{
+		times: [{ starting_time: 1355761910000, ending_time: 1355761910000 }],
+	},
+	{
+		times: [{ starting_time: 1355761920000, ending_time: 1355761920000 }],
+	},
+];
