f5f45f35aa74ee0ce4b670014c33b7e09590853a
Steven Choi <steven.m.choi@gmail.com>
2018-12-13 10:00:38 -0800

    Refactor into App.js to use React

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import testData from './db/test_data';
+
+import * as d3 from 'd3';
+import * as d3_tl from 'd3-timelines';
+
+window.d3_tl = d3_tl;
+
+var chart = d3_tl
+	.timelines()
+	.display('circle')
+	.hover((d, i, datum) => {
+		console.log('d: ', d);
+		console.log('i: ', i);
+		console.log('datum: ', datum);
+	});
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		d3.select('#root')
+			.append('svg')
+			.attr('width', 800)
+			.datum(testData)
+			.call(chart);
+
+		return null;
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,34 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import testData from './db/test_data';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-import * as d3 from 'd3';
-import * as d3_tl from 'd3-timelines';
-
-var chart = d3_tl
-	.timelines()
-	.tickFormat({
-		format: d3.timeFormat('%I %p'),
-		tickTime: d3.timeHours,
-		tickInterval: 1,
-		tickSize: 30,
-	})
-	.display('circle')
-	.hover((d, i, datum) => {
-		console.log('d: ', d);
-		console.log('i: ', i);
-		console.log('datum: ', datum);
-	});
-
-d3.select('#timeline')
-	.append('svg')
-	.attr('width', 800)
-	.datum(testData)
-	.call(chart);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
